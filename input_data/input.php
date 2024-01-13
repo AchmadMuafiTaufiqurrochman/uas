dedaa3a051311ae74afd44ec32f579b8bf61b7aa
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2024-01-13 08:17:14 +0700

    navbar

--- input_data/input.php
+++ input_data/input.php
@@ -3,15 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style src="../style.css"></style>
+    <title>Input Data</title>
+    <link rel="stylesheet" type="text/css" href="./style1.css">
+        
 </head>
 <body>
     <nav>
-        
+        <ul>
+            <li>
+                <a href="#">
+                    <img src="./unicorn.png" alt="unicorn">
+                    <span class="nav-item">UNICORN</span>
+                </a>
+            </li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
     </nav>
-    <main>
-        
-    </main>
 </body>
 </html>
