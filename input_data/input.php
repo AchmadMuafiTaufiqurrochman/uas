2d95c6b5cc6aaed2610689fc241ea205d6df5f69
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2024-01-14 12:17:25 +0700

    kotak konten jadi

--- input_data/input.php
+++ input_data/input.php
@@ -64,26 +64,21 @@
   </li>
 </ul>
   </div>
+ 
   <section class="home-section">
+ 
+
     <div class="home-content">
       <i class='bx bx-menu' ></i>
       <span class="text">Input Data Pelanggan</span>
+      <div class="content-main">
+          halo
+        </div>
     </div>
+  
   </section>
-  <script>
-  let arrow = document.querySelectorAll(".arrow");
-  for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=>{
-   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-   arrowParent.classList.toggle("showMenu");
-    });
-  }
-  let sidebar = document.querySelector(".sidebar");
-  let sidebarBtn = document.querySelector(".bx-menu");
-  console.log(sidebarBtn);
-  sidebarBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
-  });
+
+  <script src="./script.js">
   </script>
 </body>
 </html>
