380c164f96fd55bc32a3440f6c6d1d7b62c2a624
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2024-01-14 11:25:34 +0700

    navbar jadi

--- input_data/input.php
+++ input_data/input.php
@@ -1,25 +1,89 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<!-- Coding by CodingNepal | www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+  <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Input Data</title>
-    <link rel="stylesheet" type="text/css" href="./style1.css">
-        
-</head>
+    <title> Drop Down Sidebar Menu | CodingLab </title>
+    <link rel="stylesheet" href="./style1.css">
+    <!-- Boxiocns CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
 <body>
-    <nav>
-        <ul>
-            <li>
-                <a href="#">
-                    <img src="./unicorn.png" alt="unicorn">
-                    <span class="nav-item">UNICORN</span>
-                </a>
-            </li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+  <div class="sidebar close">
+    <div class="logo-details">
+      <img src="unicorn.png" alt="" class="image">
+      <span class="logo_name">UNNICORN</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Data Master</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Data Master</a></li>
         </ul>
-    </nav>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-collection' ></i>
+            <span class="link_name">Input Data</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Input Data</a></li>
+          <li><a href="#">Input Data Pelanggan</a></li>
+          <li><a href="#">Input Data Barang</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class='bx bx-pie-chart-alt-2' ></i>
+          <span class="link_name">Nota</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Nota</a></li>
+        </ul>
+      </li>
+
+
+      <li>
+    <div class="profile-details">
+      <div class="profile-content">
+      </div>
+      <div class="name-job">
+        <div class="profile_name">Admin</div>
+        <div class="job"></div>
+      </div>
+      <i class= ></i>
+    </div>
+  </li>
+</ul>
+  </div>
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu' ></i>
+      <span class="text">Input Data Pelanggan</span>
+    </div>
+  </section>
+  <script>
+  let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });
+  </script>
 </body>
 </html>
